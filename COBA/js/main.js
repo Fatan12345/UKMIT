3706e5334f0f0c72c54a0f3685815bff32ca2a4c
Fatan12345 <fatanhardcore@gmail.com>
2016-07-25 11:13:00 +0700

    project UKMIT
    version 1

---
+++ COBA/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+     
+	$("#owl-demo").owlCarousel({
+     
+		navigation : false, // menunjukkan sebuah next dan frevious buttonnya  
+		singleItemlideSpeed : 300,
+		paginationSpeed : 400,
+		singleItem: true,
+		autoPlay: true
+	});
+     
+});
